3b26b27be020d77e23199b82ac07045d172d6564
ArghgrA <0xarghh@gmail.com>
2026-02-07 12:12:54 +0100

    Aggiunta Inizializzazione Progetto tramite Spring Initializer

---
+++ src/test/java/com/github/ArghgrA/Hackhub/HackhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ArghgrA.Hackhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
