664619bb52b519f473ba8d397a9f2f46d5099cda
ArghgrA <0xarghh@gmail.com>
2026-02-07 18:00:07 +0100

    Aggiunta java doc in Hackathon e HackathonBuilder

--- src/main/java/com/github/ArghgrA/Hackhub/model/hackathon/Hackathon.java
+++ src/main/java/com/github/ArghgrA/Hackhub/model/hackathon/Hackathon.java
@@ -9,18 +9,75 @@ import lombok.Setter;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Rappresenta un Hackathon all'interno del dominio dell'applicazione.
+ * <p>
+ * Questa classe contiene tutte le informazioni principali relative
+ * a un hackathon, incluse le caratteristiche generali, la posizione,
+ * il premio, il periodo di svolgimento e lo staff coinvolto.
+ * </p>
+ *
+ * <p>
+ * Ogni hackathon è identificato univocamente tramite un {@link UUID}.
+ * Alcuni attributi, come il giudice e i mentori, possono essere
+ * modificati anche dopo la creazione dell'oggetto.
+ * </p>
+ */
 @Getter
 @AllArgsConstructor
 public class Hackathon {
+
+    /**
+     * Identificatore univoco dell'hackathon.
+     */
     private UUID id;
+
+    /**
+     * Nome dell'hackathon.
+     */
     private String nome;
+
+    /**
+     * Regolamento ufficiale dell'hackathon.
+     */
     private String regolamento;
+
+    /**
+     * Posizione in cui si svolge l'hackathon.
+     */
     private Posizione posizione;
+
+    /**
+     * Premio associato all'hackathon.
+     */
     private Premio premio;
+
+    /**
+     * Intervallo temporale di svolgimento dell'hackathon.
+     */
     private Intervallo intervallo;
+
+    /**
+     * Numero massimo di membri consentiti per ciascun team.
+     */
     private int maxNumMebri;
+
+    /**
+     * Giudice assegnato all'hackathon.
+     * <p>
+     * Può essere impostato o modificato dopo la creazione dell'istanza.
+     * </p>
+     */
     @Setter
     private Giudice giudice;
+
+    /**
+     * Lista dei mentori associati all'hackathon.
+     * <p>
+     * Può essere aggiornata dopo la creazione dell'oggetto.
+     * </p>
+     */
     @Setter
     private List<Mentore> mentori;
 }
+
