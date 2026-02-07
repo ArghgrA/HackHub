0f114261b480cb12c0c7aa45fe71ce4d3f337234
christian-genovesi <christian.genovesi@studenti.unicam.it>
2026-02-07 15:45:20 +0100

    cambio campo []Mentore in List<Mentore> in Hackathon

--- src/main/java/com/github/ArghgrA/Hackhub/Model/Hackathon/Hackathon.java
+++ src/main/java/com/github/ArghgrA/Hackhub/Model/Hackathon/Hackathon.java
@@ -6,21 +6,21 @@ import com.github.ArghgrA.Hackhub.Model.Utente.Staff.Mentore;
 import com.github.ArghgrA.Hackhub.Model.Premio.Premio;
 import lombok.Getter;
 import lombok.Setter;
-
+import java.util.List;
 import java.util.UUID;
+
 @Getter
 public class Hackathon {
     private UUID id;
     private String nome;
     private String regolamento;
     private Posizione posizione;
-
     private Premio premio;
     private Intervallo intervallo;
     private int maxNumMebri;
     @Setter
     private Giudice giudice;
     @Setter
-    private Mentore[] mentori;
+    private List<Mentore> mentori;
 
 }
